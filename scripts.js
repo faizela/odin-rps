f23b76926a0a058abba0706ab740eccddb33bdad
Faisal Ahmed <feisala132@gmail.com>
2025-06-16 21:12:39 +0100

    added getcomputerchoice func

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
 function getComputerChoice() {
     // plan how to get random choices uses math.random without arrays
+    // no input goal return either rock or paper or scissors 
+    // we only used conditionals so far 
+    // research math.random 
+    let randomInt = Math.floor(Math.random() * 3) + 1
+    if (randomInt === 1) {
+        return 'rock'
+    }
+    else if (randomInt === 2) {
+        return 'paper'
+    }
+    else {
+        return 'scissors'
+    }
 }

74f5a92291678a7d6b4ddc506a7f1180aceb665e
Faisal Ahmed <feisala132@gmail.com>
2025-06-17 02:45:39 +0100

    Added playround func

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+let humanScore = 0
+let computerScore = 0
+
+
+
 function getComputerChoice() {
     // plan how to get random choices uses math.random without arrays
     // no input goal return either rock or paper or scissors 
@@ -16,16 +21,71 @@ function getComputerChoice() {
 }
 
 
-function getHumanChoice(userChoice) {
+function getHumanChoice() {
     /*
-   input get user choice by prompt as parameter
+  
    return user choice
    for now per top instructions assume user
    enter correct input
     */
-   return userChoice
+   let userPrompt = prompt("enter rock or paper or scissors")
+   return userPrompt.toLowerCase()
 }
 
-let userPrompt = prompt("enter rock or paper or scissors")
 
 
+function playRound(humanChoice, computerChoice) {
+// use humanchoice and computerchoice as args
+// console.log winner with msg
+// increment the humscore and cpuscore based on winner
+if (humanChoice === 'rock' && computerChoice === 'rock'
+    || humanChoice === 'paper'  && computerChoice === 'paper'
+    || humanChoice === 'scissors' && computerChoice === 'scissors') {
+     console.log('It\'s a tie')
+    }
+
+else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+    console.log('Human wins - Rock beats scissors')
+    humanScore++
+}
+
+else if (humanChoice === 'paper' && computerChoice === 'rock') {
+    console.log('Human wins - paper beats rock')
+    humanScore++
+}
+else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+    console.log('Human wins - scissors beats paper')
+    humanScore++
+}
+
+else if (computerChoice === 'rock' && humanChoice === 'scissors') {
+    console.log('CPU wins - Rock beats scissors')
+    computerScore++
+}
+
+else if (computerChoice === 'paper' && humanChoice === 'rock') {
+    console.log('CPU wins - paper beats rock')
+    computerScore++
+}  
+
+else if (computerChoice === 'scissors' && humanChoice === 'paper') {
+    console.log('CPU wins - scissors beats paper')
+     computerScore++
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+const humanSelection = getHumanChoice()
+const cpuSelection = getComputerChoice()
+console.log(cpuSelection)
+console.log(playRound(humanSelection, cpuSelection))
